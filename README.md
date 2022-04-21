46f1eaab8231580caab7db23617280c1687e5f99
Misha <mishkamba1@gmail.com>
2022-04-21 14:57:07 +0900

    models init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # arctic-knowledge
 ## Конкурсный проект команды Pulse по МПИТ-2022
+Использованный стек технологий:
+- python(Django);
+- HTML/CSS.
--- webProject/web/models.py
+++ webProject/web/models.py
@@ -1,3 +1,19 @@
+from pyexpat import model
+from statistics import mode
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+class Ethnos(models.Model):
+    name = models.CharField(max_length=255)
+    brief_text = models.TextField("")
+    def __str__(self):
+        return self.name
+class Article(models.Model):
+    head = models.CharField(max_length=255)
+    body = models.TextField("")
+    image = models.ImageField(blank=True)
+    date = models.DateTimeField(auto_now=True)
+    ethnos = models.ForeignKey(Ethnos, on_delete=models.CASCADE, default=0)
+    def __str__(self):
+        return self.head

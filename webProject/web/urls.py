c5b0c282ca496aa0e349db127dc378b95bf05d25
Misha <mishkamba1@gmail.com>
2022-04-21 15:37:43 +0900

    routes and responses init

--- webProject/web/urls.py
+++ webProject/web/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path("ethnos", views.ethnoses),
+    path("ethnos/<int:id>", views.ethnos),
+    path("ethnos/<int:ethnosId>/article/<int:articleId>", views.article)
 ]
--- webProject/web/views.py
+++ webProject/web/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Ethnos, Article
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+def ethnoses(request):
+    data = Ethnos.objects.all()
+    return render(request, "ethnoses.html", {"eth": data})
+def ethnos(request, id):
+    data = Article.objects.filter(ethnos__id=id)
+    return render(request, "ethnos.html", {"data": data})
+def article(request, ethnosId, articleId):
+    data = Article.objects.get(id=articleId)
+    return render(request, "article.html", {"data": data})
